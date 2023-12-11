446ae1a55b38c388043c16750292bd8cd5e43f3f
AlvaroBonillaLledó <alvaro.bonilla.lledo@gmail.com>
2023-12-11 12:46:29 +0100

    final

---
+++ ExamenAD-parte1-Dic23-master/src/main/java/cesur/examen/core/worker/Worker.java
@@ -0,0 +1,27 @@
+package cesur.examen.core.worker;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * EXAMEN DE ACCESO A DATOS
+ * Diciembre 2023
+ *
+ * Nombre del alumno: Álvaro Bonilla Lledó
+ * Fecha: 11/12/2023
+ */
+@Data
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private String dni;
+    private Date from;
+
+
+
+
+
+}
